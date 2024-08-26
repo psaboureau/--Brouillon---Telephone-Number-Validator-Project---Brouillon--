944bca2c34517b89be0d66b80e024585fee02447
psaboureau <psaboureau@gmx.fr>
2024-08-26 21:26:32 +0200

    Finished the script on to the styling

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const input = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
 const resultsDiv = document.getElementById("results-div");
 
-
 const checkFormat = (str) => {
-    const regex = /1?\s?(\(\d{3}\)|\d{3})([-\s])?\d{3}([-|\s])?\d{4}/;
-    return regex.test(str);
-}
+  const regex = /^1?\s?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
 
-
-
+  return regex.test(str);
+};
 
 const checkInput = (str) => {
-    if (str.length === 0) {
-        alert("Please provide a phone number");
-        return;
-    }
+  if (str.length === 0) {
+    alert("Please provide a phone number");
+    return;
+  }
 
-    const p = document.createElement("p");
-    p.className = "results-text";
+  const p = document.createElement("p");
+  p.className = "results-text";
 
+  if (checkFormat(str)) {
+    p.textContent = `Valid US number: ${str}`;
+    p.classList.add("valid-results");
+  } else {
+    p.textContent = `Invalid US number: ${str}`;
+    p.classList.add("invalid-results");
+  }
 
+  input.value = "";
+  resultsDiv.appendChild(p);
+};
 
-    if (checkFormat(str)) {
-        p.textContent = `Valid US number: ${str}`;
-        p.classList.add("valid-results");
-    }
-    else {
-        p.textContent = `Invalid US number: ${str}`;
-        p.classList.add("invalid-results");
-    }
-
-    resultsDiv.appendChild(p);
-    
-}
+const cleanInput = () => {
+  resultsDiv.textContent = "";
+};
 
 checkBtn.addEventListener("click", () => checkInput(input.value));
+clearBtn.addEventListener("click", cleanInput);
